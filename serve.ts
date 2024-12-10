720316796ce6b4d0c6d35102ad48e33afa19bb29
rybla <henry@monkeyface.com>
2024-12-10 10:57:09 -0500

    working webapp

--- serve.ts
+++ serve.ts
@@ -4,10 +4,11 @@ const served_dir = "docs"
 Bun.serve({
   port,
   async fetch(req) {
-    const url = new URL(req.url);
+    const url_str = req.url.endsWith("/") ? `${req.url}index.html` : req.url
+    const url = new URL(url_str);
     const filePath = `${served_dir}${url.pathname}`;
     const file = Bun.file(filePath);
-    if (!(await file.exists())) return new Response(`Not Found: ${req.url}`, { status: 404 });
+    if (!(await file.exists())) return new Response(`Not Found: ${url_str}`, { status: 404 });
     return new Response(file);
   }
 });
