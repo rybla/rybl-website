6b81b5dce151716eb84cbed36f9a11d918c70e00
rybla <henry@monkeyface.com>
2025-02-02 22:09:05 -0500

    updated Full Example #1

--- src/Rybl/Compile/NamedDocs.purs
+++ src/Rybl/Compile/NamedDocs.purs
@@ -9,89 +9,102 @@ import Data.Map as Map
 import Data.Newtype (wrap)
 import Data.Tuple (Tuple(..))
 import Data.Unfoldable (none)
-import Rybl.Language (Doc)
-import Rybl.Language as Doc
+import Rybl.Data.Variant (inj')
+import Rybl.Language (Doc, link_external, link_internal, paragraph, ref, section, sentence, sidenote, string, string_style)
 
 namedDocs :: Map String Doc
 namedDocs = Map.fromFoldable
   [ Tuple "index" $
-      Doc.ref (wrap "full_example_1")
+      ref (wrap "full_example_1")
   , Tuple "full_example_1" $
-      Doc.section "Full Example #1"
-        [ Doc.paragraph [ Doc.sentence [ Doc.string "This is a paragraph before a section." ] ]
-        , Doc.section "Links"
-            [ Doc.paragraph
-                [ Doc.sentence
-                    [ Doc.string "Here is a link to an external page: "
-                    , Doc.link_external (Doc.string "google") { href: "https://www.google.com/", mb_favicon_src: none }
-                    , Doc.string "."
+      section "Full Example #1"
+        [ section "String Styles"
+            [ paragraph
+                [ sentence
+                    [ string "This is an emphasized string: "
+                    , string_style (inj' @"emphasis" unit) "roar"
                     ]
                 ]
-            , Doc.paragraph
-                [ Doc.sentence
-                    [ Doc.string "And now here's a link to an internal page: "
-                    , Doc.link_internal (Doc.string "lorem ipsum") { refId: wrap "lorem_ipsum" }
-                    , Doc.string "."
+            , paragraph
+                [ sentence
+                    [ string "This is a code string: "
+                    , string_style (inj' @"code" unit) "x&&2 and one Class123 + $"
                     ]
                 ]
             ]
-        , Doc.section "Sidenotes"
-            [ Doc.paragraph
-                [ Doc.sentence
-                    [ Doc.string "I wanted to say a "
-                    , Doc.sidenote
-                        (Doc.string "thing")
-                        (Doc.paragraph [ Doc.sentence [ Doc.string "This is the longer thing that I wanted to say, because it didn't quite fit in that tiny little space that was provided." ] ])
-                    , Doc.string ", but turns out it's a little too long to say right here."
+        , section "Links"
+            [ paragraph
+                [ sentence
+                    [ string "Here is a link to an external page: "
+                    , link_external (string "google") { href: "https://www.google.com/", mb_favicon_src: none }
+                    , string "."
                     ]
-                , Doc.sentence [ Doc.string "And this is another sentence." ]
+                ]
+            , paragraph
+                [ sentence
+                    [ string "And now here's a link to an internal page: "
+                    , link_internal (string "lorem ipsum") { refId: wrap "lorem_ipsum" }
+                    , string "."
+                    ]
                 ]
             ]
-        , Doc.section "Tree"
+        , section "Sidenotes"
+            [ paragraph
+                [ sentence
+                    [ string "I wanted to say a "
+                    , sidenote
+                        (string "thing")
+                        (paragraph [ sentence [ string "This is the longer thing that I wanted to say, because it didn't quite fit in that tiny little space that was provided." ] ])
+                    , string ", but turns out it's a little too long to say right here."
+                    ]
+                , sentence [ string "And this is another sentence." ]
+                ]
+            ]
+        , section "Tree"
             ( make_section_tree 4 4
-                [ Doc.paragraph [ Doc.sentence [ Doc.string "Test" ] ] ]
+                [ paragraph [ sentence [ string "Test" ] ] ]
             )
         ]
   , Tuple "lorem_ipsum" $
-      Doc.paragraph
-        [ Doc.sentence [ Doc.string "Lorem ipsum dolor sit amet consectetur adipisicing elit." ]
-        , Doc.sentence [ Doc.string "Iure facilis, consequuntur necessitatibus aliquid ex nemo quos dolore, dicta ea possimus ratione cupiditate magni, saepe nulla odio odit aperiam incidunt eligendi!" ]
-        , Doc.sentence [ Doc.string "Lorem ipsum dolor sit amet consectetur adipisicing elit." ]
-        , Doc.sentence [ Doc.string "Iure facilis, consequuntur necessitatibus aliquid ex nemo quos dolore, dicta ea possimus ratione cupiditate magni, saepe nulla odio odit aperiam incidunt eligendi!" ]
+      paragraph
+        [ sentence [ string "Lorem ipsum dolor sit amet consectetur adipisicing elit." ]
+        , sentence [ string "Iure facilis, consequuntur necessitatibus aliquid ex nemo quos dolore, dicta ea possimus ratione cupiditate magni, saepe nulla odio odit aperiam incidunt eligendi!" ]
+        , sentence [ string "Lorem ipsum dolor sit amet consectetur adipisicing elit." ]
+        , sentence [ string "Iure facilis, consequuntur necessitatibus aliquid ex nemo quos dolore, dicta ea possimus ratione cupiditate magni, saepe nulla odio odit aperiam incidunt eligendi!" ]
         ]
   , Tuple "sidenote_example_1" $
-      Doc.section "Sidenote Example #1"
-        [ Doc.paragraph
-            [ Doc.sentence
-                [ Doc.string "I wanted to say a "
-                , Doc.sidenote
-                    (Doc.string "thing")
-                    (Doc.paragraph [ Doc.sentence [ Doc.string "This is the longer thing that I wanted to say, because it didn't quite fit in that tiny little space that was provided." ] ])
-                , Doc.string ", but turns out it's a little too long to say right here."
+      section "Sidenote Example #1"
+        [ paragraph
+            [ sentence
+                [ string "I wanted to say a "
+                , sidenote
+                    (string "thing")
+                    (paragraph [ sentence [ string "This is the longer thing that I wanted to say, because it didn't quite fit in that tiny little space that was provided." ] ])
+                , string ", but turns out it's a little too long to say right here."
                 ]
             ]
         ]
   , Tuple "example_index_4"
-      $ Doc.section "This is example_index_4 which is the title of the doc"
+      $ section "This is example_index_4 which is the title of the doc"
       $ fold
-          [ [ Doc.paragraph
-                [ Doc.sentence
-                    [ Doc.link_internal (Doc.string "This") { refId: wrap "example_index_3" }
-                    , Doc.string " is a ref to the example_index_3."
+          [ [ paragraph
+                [ sentence
+                    [ link_internal (string "This") { refId: wrap "example_index_3" }
+                    , string " is a ref to the example_index_3."
                     ]
-                , Doc.sentence [ Doc.string "Isn't that great?" ]
-                , Doc.sentence [ Doc.string "Lorem ipsum dolor sit amet consectetur adipisicing elit." ]
-                , Doc.sentence [ Doc.string "Iure facilis, consequuntur necessitatibus aliquid ex nemo quos dolore, dicta ea possimus ratione cupiditate magni, saepe nulla odio odit aperiam incidunt eligendi!" ]
-                , Doc.sentence [ Doc.string "Lorem ipsum dolor sit amet consectetur adipisicing elit." ]
-                , Doc.sentence [ Doc.string "Iure facilis, consequuntur necessitatibus aliquid ex nemo quos dolore, dicta ea possimus ratione cupiditate magni, saepe nulla odio odit aperiam incidunt eligendi!" ]
+                , sentence [ string "Isn't that great?" ]
+                , sentence [ string "Lorem ipsum dolor sit amet consectetur adipisicing elit." ]
+                , sentence [ string "Iure facilis, consequuntur necessitatibus aliquid ex nemo quos dolore, dicta ea possimus ratione cupiditate magni, saepe nulla odio odit aperiam incidunt eligendi!" ]
+                , sentence [ string "Lorem ipsum dolor sit amet consectetur adipisicing elit." ]
+                , sentence [ string "Iure facilis, consequuntur necessitatibus aliquid ex nemo quos dolore, dicta ea possimus ratione cupiditate magni, saepe nulla odio odit aperiam incidunt eligendi!" ]
                 ]
             ]
           , make_section_tree 4 4
-              [ Doc.paragraph
-                  [ Doc.sentence [ Doc.string "Lorem ipsum dolor sit amet consectetur adipisicing elit." ]
-                  , Doc.sentence [ Doc.string "Iure facilis, consequuntur necessitatibus aliquid ex nemo quos dolore, dicta ea possimus ratione cupiditate magni, saepe nulla odio odit aperiam incidunt eligendi!" ]
-                  , Doc.sentence [ Doc.string "Lorem ipsum dolor sit amet consectetur adipisicing elit." ]
-                  , Doc.sentence [ Doc.string "Iure facilis, consequuntur necessitatibus aliquid ex nemo quos dolore, dicta ea possimus ratione cupiditate magni, saepe nulla odio odit aperiam incidunt eligendi!" ]
+              [ paragraph
+                  [ sentence [ string "Lorem ipsum dolor sit amet consectetur adipisicing elit." ]
+                  , sentence [ string "Iure facilis, consequuntur necessitatibus aliquid ex nemo quos dolore, dicta ea possimus ratione cupiditate magni, saepe nulla odio odit aperiam incidunt eligendi!" ]
+                  , sentence [ string "Lorem ipsum dolor sit amet consectetur adipisicing elit." ]
+                  , sentence [ string "Iure facilis, consequuntur necessitatibus aliquid ex nemo quos dolore, dicta ea possimus ratione cupiditate magni, saepe nulla odio odit aperiam incidunt eligendi!" ]
                   ]
               ]
           ]
@@ -104,6 +117,6 @@ make_section_tree depth breadth body =
   else
     body <>
       Array.replicate breadth
-        ( Doc.section "Section Title"
+        ( section "Section Title"
             (body <> make_section_tree (depth - 1) breadth body)
         )
